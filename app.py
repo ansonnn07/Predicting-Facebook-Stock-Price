d81e6acd74b475f0ee55985155f133597ae4f85e
Anson <roxastan@hotmail.com>
2021-05-04 23:23:22 +0800

    rearranged files and added plots to app

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from PIL import Image
 import yfinance as yf
 import plotly.graph_objs as go
 import cufflinks as cf
+import pickle
 
 
 register_matplotlib_converters()
@@ -33,14 +34,16 @@ tf.random.set_seed(42)
 # Add title, descriptions and image
 st.title('COVID-19 Malaysia')
 st.markdown('''
-**Description**
-
-The Facebook stock data is obtained directly through Yahoo Finance API and they are updated live.
+The Facebook stock data is obtained directly through Yahoo Finance API and they provide live updates.
+This app is built to show the application of some time series forecasting models.
+The live updates to the Facebook stock data can also be seen here,
+with Bollinger Bands provided, as Bollinger Bands is one of the methods used to determine whether
+it is beneficial to buy or sell at a given time.
 
 - App built by [Anson](https://www.linkedin.com/in/ansonnn07/)
 - Built with `Python`, using `streamlit`, `yfinance`, `pandas`, `numpy`, `matplotlib`
 
-**Links to my profiles**: [GitHub](https://github.com/ansonnn07/covid19-malaysia), 
+**Links**: [GitHub](https://github.com/ansonnn07/Predicting-Facebook-Stock-Price), 
 [LinkedIn](https://www.linkedin.com/in/ansonnn07/),
 [Kaggle](https://www.kaggle.com/ansonnn/code)
 ''')
@@ -67,21 +70,39 @@ st.info(companyInfo)
 
 @st.cache
 def read_fb_stock():
-    df = pd.read_csv('fb_close.csv', index_col=[0], parse_dates=[0])
-    return df
+    return pd.read_csv('data//fb_close.csv', index_col=[0], parse_dates=[0])
+
+
+def load_model():
+    return keras.models.load_model('data//LSTM_model.h5')
+
+
+@st.cache
+def load_scaler():
+    return pickle.load(open('data//scaler.pkl', 'rb'))
+
+
+@st.cache
+def load_results():
+    return pd.read_csv('data//full_results.csv', index_col=[0], parse_dates=[0])
 
 
 df = read_fb_stock()
+model = load_model()
+scaler = load_scaler()
+results_df = load_results()
 
 full_df = yf.download(tickerSymbol)
 st.header("**Facebook Stock Data History**")
 full_styled = full_df.copy()
 full_styled.index = full_styled.index.astype(str)
+full_styled = full_styled.style.format('{0:,.2f}')
 st.dataframe(full_styled)
+
 
 fig = full_df['Close'].plot(title='Closing Price History of Facebook')
 fig.update_layout(showlegend=False, xaxis_title=None,
-                  yaxis_title=None, height=500)
+                  yaxis_title=None, height=600)
 fig.update_xaxes(
     rangeslider_visible=False,
     rangeselector=dict(
@@ -97,7 +118,42 @@ fig.update_xaxes(
 )
 st.plotly_chart(fig, use_container_width=True)
 
-st.header("**Ticker Data**")
+st.header("**Forecasting Results**")
+results_df.rename(columns={'Close': 'Closing Price',
+                           'LSTM_pred': 'LSTM Predictions',
+                           'LSTM_future': 'LSTM Future Forecast'}, inplace=True)
+fig = results_df.iloc[-365:].plot(title='Predicting Facebook Stock Closing Price\n'
+                                  'using Different Models')
+fig.update_layout(height=600,
+                  xaxis_title=None, yaxis_title=None,
+                  hovermode='x',
+                  legend=dict(title=None,
+                              yanchor="top",
+                              y=0.99,
+                              xanchor="left",
+                              x=0.01
+                              ))
+fig.update_traces(line_width=1.6, hovertemplate=None)
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("""
+**Explanation for the predictive models**
+
+- ARIMA and SARIMAX (red and green lines) are common machine learning models used for time series forecasting.
+In this case, they did not perform so well as compared to the other models.
+- Prophet is another type of model developed by Facebook themselves, but also did not perform as well,
+because Prophet is designed specifically for time series that has very obvious seasonality characteristics and consistent overall average,
+unlike the current time series, where there is a clear trend of increasing upwards.
+- LSTM (Long Short Term Memory) is a deep learning model, more specifically a type of Recurrent Neural Networks.
+it shows promising results predicting the overall values of the stock price, 
+with a root mean square error of 9.2134 on the **predictions** when given the ***actual*** data.
+This is because the LSTM in this case uses **previous 10 days** to predict ***one day*** into the future, 
+therefore, the results will be very close to the original closing price.
+When forecasting the stock price of the future 30 days (blue lines) using only the data of 10 days, 
+it does show acceptable results but it has to be validated later when the actual data has been obtained.
+""")
+
+st.header("**Data Today**")
 today_df = yf.download(tickerSymbol, period='1d', interval='1m')
 fig = go.Figure(data=[go.Candlestick(x=today_df.index,
                 open=today_df['Open'],
@@ -127,8 +183,25 @@ fig.update_xaxes(
 )
 st.plotly_chart(fig, use_container_width=True)
 
-st.header('**Bollinger Bands**')
-qf = cf.QuantFig(today_df, title='First Quant Figure', legend='top', name='GS')
+st.subheader('**Bollinger Bands**')
+qf = cf.QuantFig(today_df, legend='top', name='GS')
 qf.add_bollinger_bands()
 fig = qf.iplot(asFigure=True)
+st.plotly_chart(fig, use_container_width=True)
+
+df_roll = df['Close'].rolling(7, min_periods=1, center=True).mean()
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df.index, y=df['Close'], name="Close",
+                         mode='markers', marker_size=3))
+fig.add_trace(go.Scatter(x=df_roll.index, y=df_roll.values, name="Average",
+                         line_width=1.6))
+fig.update_layout(title='Daily Closing Price VS 7-day Moving Average',
+                  hovermode='x',
+                  height=600,
+                  legend=dict(
+                      yanchor="top",
+                      y=0.99,
+                      xanchor="left",
+                      x=0.01
+                  ))
 st.plotly_chart(fig, use_container_width=True)
